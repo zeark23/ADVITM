ec40c02f21c92ec18adda6587807f33ccb74c253
zeark23 <zeark23@gmail.com>
2025-10-07 20:30:08 -0500

    Corrección: mitigacion Code Injection 802

--- vulnerabilities/view_source.php
+++ vulnerabilities/view_source.php
@@ -1,103 +1,66 @@
 <?php
-
 define( 'DVWA_WEB_PAGE_TO_ROOT', '../' );
 require_once DVWA_WEB_PAGE_TO_ROOT . 'dvwa/includes/dvwaPage.inc.php';
 
 dvwaPageStartup( array( 'authenticated' ) );
 
 $page = dvwaPageNewGrab();
-$page[ 'title' ] .= 'Source' . $page[ 'title_separator' ].$page[ 'title' ];
+$page[ 'title' ] = 'Help' . $page[ 'title_separator' ].$page[ 'title' ];
 
-if (array_key_exists ("id", $_GET) && array_key_exists ("security", $_GET)) {
+if (array_key_exists ("id", $_GET) &&
+	array_key_exists ("security", $_GET) &&
+	array_key_exists ("locale", $_GET)) {
+	
 	$id       = $_GET[ 'id' ];
 	$security = $_GET[ 'security' ];
-
-
-	switch ($id) {
-		case "fi" :
-			$vuln = 'File Inclusion';
-			break;
-		case "brute" :
-			$vuln = 'Brute Force';
-			break;
-		case "csrf" :
-			$vuln = 'CSRF';
-			break;
-		case "exec" :
-			$vuln = 'Command Injection';
-			break;
-		case "sqli" :
-			$vuln = 'SQL Injection';
-			break;
-		case "sqli_blind" :
-			$vuln = 'SQL Injection (Blind)';
-			break;
-		case "upload" :
-			$vuln = 'File Upload';
-			break;
-		case "xss_r" :
-			$vuln = 'Reflected XSS';
-			break;
-		case "xss_s" :
-			$vuln = 'Stored XSS';
-			break;
-		case "weak_id" :
-			$vuln = 'Weak Session IDs';
-			break;
-		case "javascript" :
-			$vuln = 'JavaScript';
-			break;
-		case "authbypass" :
-			$vuln = 'Authorisation Bypass';
-			break;
-		case "open_redirect" :
-			$vuln = 'Open HTTP Redirect';
-			break;
-		default:
-			$vuln = "Unknown Vulnerability";
+	$locale   = $_GET[ 'locale' ];
+	
+	// Sanitizar entradas para prevenir Path Traversal
+	$id = basename($id);
+	$id = preg_replace('/[^a-zA-Z0-9_-]/', '', $id);
+	
+	$locale = basename($locale);
+	$locale = preg_replace('/[^a-zA-Z_]/', '', $locale);
+	
+	// Lista blanca de IDs permitidos
+	$allowed_ids = array('brute', 'exec', 'csrf', 'fi', 'sqli', 'sqli_blind', 'upload', 'xss_d', 'xss_r', 'xss_s');
+	$allowed_locales = array('en', 'es', 'fr', 'de', 'zh');
+	
+	if (!in_array($id, $allowed_ids)) {
+		$help = "<p>Invalid help page requested</p>";
+	} elseif (!in_array($locale, $allowed_locales)) {
+		$locale = 'en'; // Default a inglés
 	}
-
-	$source = @file_get_contents( DVWA_WEB_PAGE_TO_ROOT . "vulnerabilities/{$id}/source/{$security}.php" );
-	$source = str_replace( array( '$html .=' ), array( 'echo' ), $source );
-
-	$js_html = "";
-	if (file_exists (DVWA_WEB_PAGE_TO_ROOT . "vulnerabilities/{$id}/source/{$security}.js")) {
-		$js_source = @file_get_contents( DVWA_WEB_PAGE_TO_ROOT . "vulnerabilities/{$id}/source/{$security}.js" );
-		$js_html = "
-		<h2>vulnerabilities/{$id}/source/{$security}.js</h2>
-		<div id=\"code\">
-			<table width='100%' bgcolor='white' style=\"border:2px #C0C0C0 solid\">
-				<tr>
-					<td><div id=\"code\">" . highlight_string( $js_source, true ) . "</div></td>
-				</tr>
-			</table>
-		</div>
-		";
+	
+	if (in_array($id, $allowed_ids)) {
+		ob_start();
+		
+		if ($locale == 'en') {
+			$help_file = DVWA_WEB_PAGE_TO_ROOT . "vulnerabilities/{$id}/help/help.php";
+		} else {
+			$help_file = DVWA_WEB_PAGE_TO_ROOT . "vulnerabilities/{$id}/help/help.{$locale}.php";
+		}
+		
+		// Verificar que el archivo existe antes de incluirlo
+		if (file_exists($help_file) && is_file($help_file)) {
+			include($help_file);
+		} else {
+			echo "<p>Help file not found</p>";
+		}
+		
+		$help = ob_get_contents();
+		ob_end_clean();
 	}
-
-	$page[ 'body' ] .= "
-	<div class=\"body_padded\">
-		<h1>{$vuln} Source</h1>
-
-		<h2>vulnerabilities/{$id}/source/{$security}.php</h2>
-		<div id=\"code\">
-			<table width='100%' bgcolor='white' style=\"border:2px #C0C0C0 solid\">
-				<tr>
-					<td><div id=\"code\">" . highlight_string( $source, true ) . "</div></td>
-				</tr>
-			</table>
-		</div>
-		{$js_html}
-		<br /> <br />
-
-		<form>
-			<input type=\"button\" value=\"Compare All Levels\" onclick=\"window.location.href='view_source_all.php?id=$id'\">
-		</form>
-	</div>\n";
 } else {
-	$page['body'] = "<p>Not found</p>";
+	$help = "<p>Not Found</p>";
 }
 
-dvwaSourceHtmlEcho( $page );
+$page[ 'body' ] .= "
+<script src='/vulnerabilities/help.js'></script>
+<link rel='stylesheet' type='text/css' href='/vulnerabilities/help.css' />
+<div class=\"body_padded\">
+	{$help}
+</div>\n";
 
+dvwaHelpHtmlEcho( $page );
 ?>
